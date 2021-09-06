35968c977a75c8943e80e31afc4e831b0721d04b
Kirill Lavrov <lkv@myttk.ru>
2021-09-06 16:41:27 +0700

    feat (script.js): Main task is completed

--- script/script.js
+++ script/script.js
@@ -50,11 +50,41 @@
   const
     popUp = document.querySelector('.popup'),
     popUpBtns = document.querySelectorAll('.popup-btn'),
-    popupClose = document.querySelector('.popup-close');
+    popupClose = document.querySelector('.popup-close'),
+    animatePopUp = delay => {
+      let count = 0;
+      const
+        animationStep = () => {
+          popUp.firstElementChild.style.transform = `translateX(-50px) scale(${count / delay})`;
+          popUp.firstElementChild.style.opacity = (1 / delay) * count;
+          count++;
+        },
+        animationReset = () => {
+          count = 0;
+          popUp.firstElementChild.style.transform = '';
+          popUp.firstElementChild.style.opacity = '';
+        },
+        animation = () => {
+          const requestId = requestAnimationFrame(animation);
+
+          if (count !== delay) {
+            animationStep();
+            return requestId;
+          } else {
+            cancelAnimationFrame(requestId);
+            animationReset();
+          }
+        };
+
+      animation();
+    };
 
   popUpBtns.forEach(item => {
     item.addEventListener('click', () => {
       popUp.style.display = 'block';
+      if (!(screen.width < 768)) {
+        animatePopUp(20);
+      }
     });
   });
   popupClose.addEventListener('click', () => {
